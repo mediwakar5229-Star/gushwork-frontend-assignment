a6fd56c26c99a198f0cbd8b01f0eb661103e9f8e
mediwakar5229-Star <mediwakar5229@gmail.com>
2026-04-08 15:28:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-window.addEventListener("scroll",()=>{header.style.top=scrollY>100?"0":"-60px"});
-
-const obs=new IntersectionObserver(e=>{e.forEach(x=>{if(x.isIntersecting)x.target.classList.add("show")})});
-document.querySelectorAll(".fade-in").forEach(el=>obs.observe(el));
-
-document.querySelectorAll(".role").forEach(r=>{
-r.addEventListener("click",()=>{
-document.querySelectorAll(".role").forEach(x=>x.classList.remove("active"));
-r.classList.add("active");
-continueBtn.disabled=false;
-});
-});
+console.log("working");
